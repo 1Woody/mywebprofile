a62715a75a7d6306d615319ec5323c66ecfb4664
[daniel.benavente4@gmail.com] <[daniel.benavente4@gmail.com]>
2020-10-16 17:57:23 +0200

    allhidememorygame

--- memorygame/js/memory_js.js
+++ memorygame/js/memory_js.js
@@ -8,10 +8,23 @@ function initial_cards(n) {
 }
 
 function MemoryGame(){
+    var flipped="fas fa-question-circle";
     var icons= ["fab fa-amazon","fab fa-android","fab fa-apple","fab fa-google","fab fa-microsoft","fab fa-facebook-square"];
-    let random_pos = initial_cards(icons.length*2);
-    for(let i=0; i<12; i++){
+    var rand_icons = initial_cards(icons.length*2);
+
+    for(let i=0; i<rand_icons.length; i++){
         $("#"+i).removeAttr("class");
-        $("#"+i).addClass(icons[random_pos[i]-1]);
+        $("#"+i).addClass(flipped);
+        document.getElementById(i).addEventListener('click', function(){
+            $("#"+i).removeAttr("class");
+            $("#"+i).addClass(icons[rand_icons[i]-1]);
+        });
     }
 }
+
+
+/*let rand_icons = [3,3,2,2,1,1,4,4,5,5,6,6];
+for(let i=0; i<12; i++){
+    $("#"+i).removeAttr("class");
+    $("#"+i).addClass(icons[rand_icons[i]-1]);
+}*/
