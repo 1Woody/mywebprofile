698f095d4811d80a3b63255efc33ec2e24fa8f00
1Woody <daniel.benavente4@gmail.com>
2020-10-18 23:27:58 +0200

    gamedone

--- memorygame/js/memory_js.js
+++ memorygame/js/memory_js.js
@@ -1,3 +1,10 @@
+//Global var
+var icons = ["fab fa-amazon","fab fa-android","fab fa-apple","fab fa-google","fab fa-microsoft","fab fa-facebook-square"];
+var flipped_icon="fas fa-question-circle";
+var cardMatrix = [];
+var flip = [];
+var wincount = 0;
+
 function initial_cards(n) {
     arrayRandom = new Array(n).fill().map((_e,i,_a) => Math.floor(i/2)+1);
     arrayRandom.forEach((_e, i, a) => { 
@@ -7,21 +14,41 @@ function initial_cards(n) {
     return arrayRandom;
 }
 
-function MemoryGame(){
-    var flipped="fas fa-question-circle";
-    var icons= ["fab fa-amazon","fab fa-android","fab fa-apple","fab fa-google","fab fa-microsoft","fab fa-facebook-square"];
-    var rand_icons = initial_cards(icons.length*2);
+function DisplayCard(i){
+    if (flip.length < 2){
+        $("#"+i).removeAttr("class") && $("#"+i).addClass(cardMatrix[i]);
+        flip.push(i);
+        if (flip.length == 2) setTimeout(function(){check();}, 500);
+    }
+}
 
-    for(let i=0; i<rand_icons.length; i++){
-        $("#"+i).removeAttr("class");
-        $("#"+i).addClass(flipped);
+function check(){
+    if (cardMatrix[flip[0]] != cardMatrix[flip[1]] || flip[0] == flip[1]){
+        for (let i in flip){ 
+            $("#"+flip[i]).removeAttr("class") && $("#"+flip[i]).addClass(flipped_icon);
+        }
+    } else{
+        wincount++;
+        if (wincount == 6){
+            alert("Congrats!! You've won!!");
+            location.reload();
+        }
+    }
+    flip = [];
+}
+
+function MemoryGame(){
+    let rand_icons = initial_cards(icons.length*2);
+    cardMatrix = [];
+    flip = [];
+    wincount = 0;
+    for(let i in rand_icons){
+        cardMatrix.push(icons[rand_icons[i]-1]);
+    }
+    for(let i in cardMatrix){
+        $("#"+i).removeAttr("class") && $("#"+i).addClass(flipped_icon);
         document.getElementById(i).addEventListener('click', function(){
-            $("#"+i).removeAttr("class");
-            $("#"+i).addClass(icons[rand_icons[i]-1]);
-            setTimeout(function(){
-                $("#"+i).removeAttr("class");
-                $("#"+i).addClass(flipped);
-            },1000);
+            DisplayCard(this.id);
         });
     }
 }
