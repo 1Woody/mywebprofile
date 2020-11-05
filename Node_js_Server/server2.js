b2ba9fb9a49a7f7d59984d84606f55fd0475b696
[daniel.benavente4@gmail.com] <[daniel.benavente4@gmail.com]>
2020-11-05 12:36:27 +0100

    newserverparams

--- Node_js_Server/server2.js
+++ Node_js_Server/server2.js
@@ -1,11 +1,16 @@
 const http = require('http');
-const hostname = '127.0.0.1';
+const url = require('url');
+const hostname = 'ubiwan.epsevg.upc.edu';
 const port = 8000;
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!');
-});
-server.listen(port, hostname, () => {
+http.createServer((req, res) => {
+    let q = url.parse(req.url, true);
+    let host = q.host;
+    let path = q.pathname;
+    let search = q.search;
+    let search_obj = q.query;
+    res.write(`host: ${host}\npath: ${path}\nsearch: ${search}\n`);
+    for (let k in search_obj) res.write(`\n${k}: ${search_obj[k]}`);
+    res.end();
+}).listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
-});
+})
